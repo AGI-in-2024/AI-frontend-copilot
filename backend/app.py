f7994eb9431c4fb45c732d3775bfdd5537ecd094
bukhanka <dukhanindaniil@yandex.ru>
2024-09-24 22:24:03 +0500

    server fix

--- backend/app.py
+++ backend/app.py
@@ -113,4 +113,4 @@ def _corsify_actual_response(response, status_code=200):
     return response, status_code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
